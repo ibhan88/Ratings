0cd061d0ad4321962cebe760e2badbaf849f254a
Hackbright Student <info@hackbrightacademy.com>
2016-08-02 18:03:59 -0700

    Changes to new user creation

--- server.py
+++ server.py
@@ -3,7 +3,7 @@
 from jinja2 import StrictUndefined
 
 from flask import Flask, render_template, redirect, request, flash, session
-from flask_debugtoolbar import DebugToolbarExtension
+# from flask_debugtoolbar import DebugToolbarExtension
 
 from model import User, Rating, Movie, connect_to_db, db
 
@@ -30,34 +30,61 @@ def index():
 def user_list():
     """Show list of users."""
 
-    users = User.query.all()
     return render_template("user_list.html", users=users)
 
 @app.route('/register')
 def show_register_form():
     """Shows registration form."""
 
-    return render_template("register_form.html")
+    existing_users = User.query.filter(User.email!=None).all()
+    emails = []   
+    for user in existing_users:
+        emails.append(user.email)
+
+    print "#################### EMAILS ######################"
+    print emails
+    print "##################################################"
+
+    return render_template("register_form.html", emails=emails)
     # if methods == "GET":
     #     return render_template("register_form.html")
     # else:
     #     #Some CODE
     #     return redirect("/")
 
+@app.route('/user_id_check')
+def check_user_id():
+
+    new_email = request.form.get("email")
+    emails = User.query.filter(User.email!=None)
+    #check password too
+    if new_email in emails:
+        return "This email is already registered."
+
+
 @app.route('/register-confirmation', methods=["POST"])
 def show_register_confirmation():
     """Shows registration confirmation."""
 
-    return render_template("registration_confirmation.html")
+    email = request.form.get("email")
+    password = request.form.get("password")
+    age = request.form.get("age")
+    zipcode = request.form.get("zipcode")
+
+    new_user = User(email=email,password=password,age=age,zipcode=zipcode)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect("/")
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
-    app.debug = True
+    # app.debug = True
 
     connect_to_db(app)
 
     # Use the DebugToolbar
-    DebugToolbarExtension(app)
+    # DebugToolbarExtension(app)
 
     app.run()
